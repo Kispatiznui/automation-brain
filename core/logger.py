05be7819c065d942789cbc8fee344e2e4a4c85bb
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-17 23:19:17 -0500

    Delete core directory

--- core/logger.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-def log(action, status, result):
-    print({
-        "time": str(datetime.datetime.now()),
-        "action": action,
-        "status": status,
-        "result": result
-    })
--- core/router.py
+++
@@ -1,6 +0,0 @@
-from app.plugins.clean_downloads import clean_downloads
-
-ACTIONS = {
-    "clean_downloads": clean_downloads
-}
-
